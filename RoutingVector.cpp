010ef8f2de731468d60f3f50c8dc982d03fd5750
Alex Grabski <101558615+Alex-G-R@users.noreply.github.com>
2024-08-13 20:09:31 +0200

    Delete RoutingVector.cpp

--- RoutingVector.cpp
+++
@@ -1,8 +0,0 @@
-
-#include "RoutingVector.h"
-
-RoutingVector::RoutingVector(std::string P_route, std::string P_file_path)
-{
-    route = std::move(P_route);
-    file_path = std::move(P_file_path);
-}
