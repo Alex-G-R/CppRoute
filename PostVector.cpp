adff856a08c3c02f9fbc47e51df07e3c320297c2
Alex Grabski <101558615+Alex-G-R@users.noreply.github.com>
2024-08-13 20:09:44 +0200

    Delete PostVector.cpp

--- PostVector.cpp
+++
@@ -1,8 +0,0 @@
-
-#include "PostVector.h"
-
-PostVector::PostVector(std::string P_post_route, std::function<std::string(std::string req_body)> func)
-{
-    post_route = std::move(P_post_route);
-    handler_function = std::move(func);
-}
