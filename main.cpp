d081094831848bb40da327c5588818d377a56500
Alex Grabski <101558615+Alex-G-R@users.noreply.github.com>
2024-08-14 18:12:26 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,20 +11,3 @@ int main() {
     return 0;
 }
 
-// Helper function to parse the request body (application/x-www-form-urlencoded)
-std::map<std::string, std::string> parseRequestBody(const std::string& req_body) {
-    std::map<std::string, std::string> params;
-    std::istringstream stream(req_body);
-    std::string key_value;
-
-    while (std::getline(stream, key_value, '&')) {
-        size_t pos = key_value.find('=');
-        if (pos != std::string::npos) {
-            std::string key = key_value.substr(0, pos);
-            const std::string value = key_value.substr(pos + 1);
-            params[key] = value;
-        }
-    }
-
-    return params;
-}
